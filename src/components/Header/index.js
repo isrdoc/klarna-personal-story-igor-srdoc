4037ee3d54655f793262e065b4872101ae591ff3
Igor Srdoc <igor.srdoc@visionect.com>
2020-12-09 17:47:15 +0100

    Add Klarna logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,10 @@
-const Header = () => <div>Header</div>
+import logo from '../../assets/img/Klarna_Logo_black.svg'
+import './index.css'
+
+const Header = () => (
+  <header>
+    <img className="logo" src={logo} alt="Klarna Logo" />
+  </header>
+)
 
 export default Header
